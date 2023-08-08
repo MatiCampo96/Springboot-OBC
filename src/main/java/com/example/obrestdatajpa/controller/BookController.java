17a5fcacaa847c8519884279fffdec3e7cb3a990
Matias Campopiano <maticampopiano@hotmail.com>
2023-08-08 18:16:31 -0300

    Edite el metodo create y estoy armando el update

--- src/main/java/com/example/obrestdatajpa/controller/BookController.java
+++ src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -1,13 +1,18 @@
-package com.example.obrestdatajpa.controller;
+  package com.example.obrestdatajpa.controller;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.obrestdatajpa.entities.Book;
@@ -15,6 +20,8 @@ import com.example.obrestdatajpa.repository.BookRepository;
 
 @RestController
 public class BookController {
+
+    private final Logger log = LoggerFactory.getLogger(BookController.class);
     private BookRepository bookRepository;
 
     
@@ -43,13 +50,28 @@ public class BookController {
 
     //Crear libro... Obtener cabeceras?? para que?? por seguridad
     @PostMapping("/api/books")
-    public Book create(@RequestBody Book book){
+    public ResponseEntity<Book> create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
         // Guardar el libro recibido por parametros en la base de datos
-        return bookRepository.save(book);
+        if(book.getId() != null){ //quiere decir que existe el id y por lo tanto no es una creacion
+            log.warn("Tryng to create a book with id");
+            System.out.println("Tryng to create a book with id");
+            return ResponseEntity.badRequest().build();
+        }
+        Book result = bookRepository.save(book);
+        return ResponseEntity.ok(result);
 
     }
 
-    //
+    /**
+     * actualizar el libro existente en base de datos
+     */
+    @PutMapping("/api/books")
+    public Book update(@RequestBody Book book){
+
+        return bookRepository.save(book);
+    }
+
+    //TODO! Me quede en el minuto 33:47 del video
     
 
     
