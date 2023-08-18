640113477e4c865c104a59366be05b459705f19c
Matias Campopiano <maticampopiano@hotmail.com>
2023-08-18 19:19:45 -0300

    Empezando con swagger

---
+++ src/main/java/com/example/obrestdatajpa/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.example.obrestdatajpa.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuracion Swagger para la generación de documentación de la API REST
+ */
+@Configuration
+public class SwaggerConfig {
+
+    public api(){
+        
+    }
+    
+    
+}
